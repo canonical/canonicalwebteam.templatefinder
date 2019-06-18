986a1b3456822b628c5436a6683cfba5f6771a3e
Robin Winslow <robin@robinwinslow.co.uk>
2019-06-18 14:57:54 +0100

    v0.1.1: Include README in package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.templatefinder",
-    version="0.1.0",
+    version="0.1.1",
     author="Canonical Webteam",
     url="https://github.com/canonical-webteam/templatefinder",
     packages=find_packages(),
@@ -12,6 +12,8 @@ setup(
         "A module which attempts to load the corresponding templates directly"
         " from URLs, without the need to write a view for each URL."
     ),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "Flask>=1.0",
         "mistune>=0.8.4",
